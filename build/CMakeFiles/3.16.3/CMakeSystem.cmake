df17d503847e863e7d78c4a4767dacf5a4f91e5f
RPK <rawinza555@hotmail.com>
2021-11-26 10:33:53 +0700

    first commit

---
+++ build/CMakeFiles/3.16.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.11.0-40-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.11.0-40-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/rawinza555/Freertos_Make/TestFreeRTOSMake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ build/CMakeFiles/NUCLEOH7_CMAKE.dir/DependInfo.cmake
@@ -0,0 +1,95 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/startup_stm32h743xx.s" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/startup_stm32h743xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32H743xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../"
+  "../home/rawinza555/Freertos_Make/TestFreeRTOSMake/PROJECT"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/eth.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/eth.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/freertos.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/freertos.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/gpio.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/gpio.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/main.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/main.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/stm32h7xx_hal_msp.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/stm32h7xx_hal_msp.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/stm32h7xx_hal_timebase_tim.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/stm32h7xx_hal_timebase_tim.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/stm32h7xx_it.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/stm32h7xx_it.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/system_stm32h7xx.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/system_stm32h7xx.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/usart.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/usart.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Core/Src/usb_otg.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Core/Src/usb_otg.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_eth.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_eth.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_eth_ex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_eth_ex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd_ex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd_ex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c.obj"
+  "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_usb.c" "/home/rawinza555/Freertos_Make/TestFreeRTOSMake/build/CMakeFiles/NUCLEOH7_CMAKE.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_usb.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32H743xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../"
+  "../home/rawinza555/Freertos_Make/TestFreeRTOSMake/PROJECT"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
